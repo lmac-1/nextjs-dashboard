bcf0f3199dd9817491dc54276fd26ef8c070eaa1
Lucy Macartney <macartney.lucy@gmail.com>
2023-11-14 10:44:38 -0500

    chapter 12 - creating an invoice

---
+++ app/lib/actions.ts
@@ -0,0 +1,42 @@
+// marks all exported functions within the file as server functions
+// created in chapter 12 https://nextjs.org/learn/dashboard-app/mutating-data
+"use server";
+
+// Zod is a Typescript-first validation library that simplifies type validation
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  // it's a good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // Next.js has a client-site router cache that stores the route segments in the user's browser for a time
+  // This clears the cache and triggers a new request to the server
+  // Once the db has been updated, the path will be revalidated and fresh data will be fetched from the server
+  revalidatePath("/dashboard/invoices");
+  // Then, we will redirect the user back to the /dashboard/invoices page
+  redirect("/dashboard/invoices");
+}
