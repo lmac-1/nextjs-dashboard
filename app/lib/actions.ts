efe0f64c0814a2edf23895932a516c6e34a25e1d
Lucy Macartney <macartney.lucy@gmail.com>
2023-11-15 11:06:30 -0500

    update invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,6 +18,9 @@ const InvoiceSchema = z.object({
 
 const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
 
+// Use Zod to update the expected types
+const UpdateInvoice = InvoiceSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
@@ -38,5 +41,24 @@ export async function createInvoice(formData: FormData) {
   // Once the db has been updated, the path will be revalidated and fresh data will be fetched from the server
   revalidatePath("/dashboard/invoices");
   // Then, we will redirect the user back to the /dashboard/invoices page
+  redirect("/dashboard/invoices");
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
